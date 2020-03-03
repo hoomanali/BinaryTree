9f4f94940bc0ca5517d6b72ba10bbb7b29143a53
Ali Hooman <me@alihooman.com>
2020-03-03 14:55:54 -0700

    Made test script output easier to read.

--- testfiles/runtest.sh
+++ testfiles/runtest.sh
@@ -6,33 +6,46 @@ CLASS='com.alihooman.BinaryTree'
 echo $CLASSPATH
 echo $CLASS
 
+echo '*************************************************'
 echo '********** Starting command line tests **********'
+echo '*************************************************'
+echo ''
+echo '*************************************************'
 echo '********** Run without arguments       **********'
+echo '*************************************************'
 echo ''
 
 java $CLASSPATH $CLASS
 wait
 
 echo ''
+echo '*************************************************'
 echo '********** Run with arguments          **********'
+echo '*************************************************'
 echo ''
 
 java $CLASSPATH $CLASS fileName
 wait
 
 echo ''
+echo '*************************************************'
 echo '********** Run with too many arguments **********'
+echo '*************************************************'
 echo ''
 
 java $CLASSPATH $CLASS Name hello
 wait
 
 echo ''
+echo '*************************************************'
 echo '********** Run with quoted arguments   **********'
+echo '*************************************************'
 echo ''
 
 java $CLASSPATH $CLASS "file Name hello"
 wait
 
 echo ''
+echo '*************************************************'
 echo '********** End command line tests      **********'
+echo '*************************************************'
