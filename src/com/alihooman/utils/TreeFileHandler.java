2824e16be6dbe98b0eaae741211dd93465eb79fe
Ali Hooman <me@alihooman.com>
2020-03-03 22:33:39 -0700

    Created setFilePath() and getFilePath() methods for TreeFileHandler class.

--- src/com/alihooman/utils/TreeFileHandler.java
+++ src/com/alihooman/utils/TreeFileHandler.java
@@ -1,4 +1,50 @@
 package com.alihooman.utils;
 
+import java.io.IOException;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class TreeFileHandler {
+    final String TAG = "[BinaryTree: TreeFileHandler] ";
+    Path path;
+
+    /**
+     * Blank constructor for TreeFileHandler class.
+     */
+    public TreeFileHandler() {
+    }
+
+    /**
+     * Sets TreeFileHandler object's path variable and checks if the path and file
+     * name are valid and exist.
+     * @param path String representation of the file name and/or absolute or relative path.
+     * @return True if path is valid and file exists.
+     */
+    public boolean setFilePath(String path) {
+        // Create path
+        this.path = Paths.get(path);
+
+        // Check if path exists
+        try {
+            this.path = this.path.toRealPath();
+        } catch(NoSuchFileException e) {
+            System.out.println(TAG + "File not found: " + path);
+            return false;
+        } catch(IOException e) {
+            System.out.println(TAG + e);
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Retrieves the TreeFileHandler object's path variable.
+     * @return The current TreeFileHandler's path, can be null.
+     */
+    public Path getFilePath() {
+        return this.path;
+    }
+
 }
